dfecf4b923ece81966401f7c176ea17e3fe82a1d
Slixe <slixeprivate@gmail.com>
2025-12-07 20:48:09 +0100

    go: add HashV3

--- go/xelis_hash.go
+++ go/xelis_hash.go
@@ -6,6 +6,7 @@ import (
 	hash "github.com/xelis-project/xelis-hash/hash"
 	v1 "github.com/xelis-project/xelis-hash/v1"
 	v2 "github.com/xelis-project/xelis-hash/v2"
+	v3 "github.com/xelis-project/xelis-hash/v3"
 )
 
 func HashV1(input []byte) (hash.Hash, error) {
@@ -23,4 +24,9 @@ func HashV1(input []byte) (hash.Hash, error) {
 func HashV2(input []byte) (hash.Hash, error) {
 	scratchPad := v2.NewScratchPad()
 	return v2.XelisHash(input, scratchPad)
+}
+
+func HashV3(input []byte) (hash.Hash, error) {
+	scratchPad := v3.NewScratchPad()
+	return v3.XelisHash(input, scratchPad)
 }
